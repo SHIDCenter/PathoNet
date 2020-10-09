97fa05499e38066b7d432a68a5128b78d7ddff85
RasoolSbz <rasools1995@gmail.com>
2020-10-09 21:42:16 +0330

    Add configs

--- config.py
+++ config.py
@@ -1,2 +1,15 @@
+class config:
+    inputShape=(256,256,3)
+    pretrainedModel=None
+    classes=3
+    model="PathoNet" #PathoNet,FRRN_A,FCRN_B,Deeplab_xception,Deeplab_mobilenet
+    logPath="logs\\"
+    checkpointsPath="checkpoints\\"
+    data_path=""
+    loss=""
+    optimizer="adam"
+    lr=1e-3
+    batchSize=16
+    epoches=30
 
     
