328e826fa4673b6d80276259ca9da6368b90f22c
RasoolSbz <rasools1995@gmail.com>
2020-10-11 18:07:10 +0330

    Update pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import ndimage
 from skimage.feature import peak_local_max
-from skimage.morphology import watershed
+from skimage.morphology import watershed as ws
 import cv2
 import models
 from  config import Config
@@ -9,14 +9,14 @@ import imageio
 import argparse
 
 
-def Watershed(pred):
+def watershed(pred):
     cells=[]
     for ch in range(3):
         gray=pred[:,:,ch]
         D = ndimage.distance_transform_edt(gray)
         localMax = peak_local_max(D, indices=False, min_distance=5,exclude_border=False,labels=gray)
         markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
-        labels = watershed(-D, markers, mask=gray)
+        labels = ws(-D, markers, mask=gray)
         for label in np.unique(labels):
             if label == 0:
                 continue
@@ -40,7 +40,7 @@ def predict(args=None):
     np.place(pred[:,:,1],pred[:,:,1]<conf.thresholds[1],0)
     np.place(pred[:,:,2],pred[:,:,2]<conf.thresholds[2],0)
     np.place(pred,pred>0,255)
-    cells=Watershed(pred)
+    cells=watershed(pred)
     return cells
 
 def get_parser():
