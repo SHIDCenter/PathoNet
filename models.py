053bbe615bce9be915613b64a4b40fb5d3177be7
Rasool Sbz <rasools1995@gmail.com>
2021-03-19 16:31:05 +0330

    Add Unet to models

--- models.py
+++ models.py
@@ -381,6 +381,80 @@ def PathoNet(input_size = (256,256,3), classes=3, pretrained_weights = None):
         model.load_weights(pretrained_weights)
     return model
 
+def Unet(pretrained_weights = None,input_size = (256,256,3)):
+    inputs = Input(input_size)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    btn1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn1)
+    btn1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(btn1)
+
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    btn2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn2)
+    btn2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(btn2)
+
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    btn3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn3)
+    btn3 = BatchNormalization()(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(btn3)
+
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    btn4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn4)
+    btn4 = BatchNormalization()(conv4)
+    #drop4 = Dropout(0.5)(btn4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(btn4)
+
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    btn5 = BatchNormalization()(conv5)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn5)
+    btn5 = BatchNormalization()(conv5)
+    #drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(btn5))
+    merge6 = concatenate([btn4,up6], axis = 3)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    btn6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn6)
+    btn6 = BatchNormalization()(conv6)
+
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(btn6))
+    merge7 = concatenate([btn3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    btn7 = BatchNormalization()(conv7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn7)
+    btn7 = BatchNormalization()(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(btn7))
+    merge8 = concatenate([btn2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    btn8 = BatchNormalization()(conv8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn8)
+    btn8 = BatchNormalization()(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(btn8))
+    merge9 = concatenate([btn1,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    btn9 = BatchNormalization()(conv9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn9)
+    btn9 = BatchNormalization()(conv9)
+    conv9 = Conv2D(8, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(btn9)
+    btn9 = BatchNormalization()(conv9)
+    conv10 = Conv2D(3, 1, activation = 'linear')(btn9)
+
+    model = Model(input = inputs, output = conv10)
+    
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+
+
 def Deeplabv3(weights=None, input_tensor=None, input_shape=(256, 256, 3), classes=3, backbone='mobilenetv2',
               OS=16, alpha=1., activation=None):
     """ Instantiates the Deeplabv3+ architecture
